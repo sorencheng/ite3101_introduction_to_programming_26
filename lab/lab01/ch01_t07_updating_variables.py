c88c015374678ffab04122236bc97d3880bd5bf6
sorencheng <sorencheng2002@gmail.com>
2026-09-14 07:05:56 +0000

    [skip actions] lab/lab01/ch01_t07_updating_variables.py

--- lab/lab01/ch01_t07_updating_variables.py
+++ lab/lab01/ch01_t07_updating_variables.py
@@ -12,4 +12,4 @@ october_rainfall = 7.20
 november_rainfall = 5.06
 december_rainfall = 4.06
 
-annual_rainfall += july_rainfall + august_rainfall + september_rainfall + october_rainfall + november_rainfall +
+annual_rainfall += july_rainfall + august_rainfall + september_rainfall + october_rainfall + november_rainfall + december_rainfall
