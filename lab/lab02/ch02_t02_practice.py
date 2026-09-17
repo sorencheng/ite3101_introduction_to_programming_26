31e8a8757ae3a150f7a8d8e8c140c4566fd0010a
sorencheng <sorencheng2002@gmail.com>
2026-09-17 07:19:50 +0000

    [skip actions] lab/lab02/ch02_t02_practice.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -1,7 +1,7 @@
 # Assign your variables below, each on its own line!
 caesar = Graham
-praline = 
-viking
+praline = John
+viking = Teresa
 # Put your variables above this line, and uncomment the following 3 line of codes.
 # print(caesar)
 # print(praline)
