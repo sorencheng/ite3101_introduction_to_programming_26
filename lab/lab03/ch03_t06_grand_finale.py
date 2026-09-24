e748551bee91f8f10d43a48f15a999ea8489bf31
sorencheng <sorencheng2002@gmail.com>
2026-09-24 08:24:36 +0000

    [skip actions] lab/lab03/ch03_t06_grand_finale.py

--- lab/lab03/ch03_t06_grand_finale.py
+++ lab/lab03/ch03_t06_grand_finale.py
@@ -10,4 +10,4 @@ current_second = now.second
 
 
 print('%02d/%02d/%04d' % (now.month, now.day, now.year))
-print('%02d%%02d:%02d' % (now.hour, now.minute, now.second))
+print('%02d:%02d:%02d' % (now.hour, now.minute, now.second))
