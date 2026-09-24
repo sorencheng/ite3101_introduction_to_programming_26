2aa12fd8a20423455411177e044577d8509bec3c
sorencheng <sorencheng2002@gmail.com>
2026-09-24 08:45:36 +0000

    [skip actions] lab/lab04/ch04_t04_how_tables_have_turned.py

--- lab/lab04/ch04_t04_how_tables_have_turned.py
+++ lab/lab04/ch04_t04_how_tables_have_turned.py
@@ -7,10 +7,10 @@ bool_one = 3 < 5  # We already did this one for you!
 bool_two = 1>2
 
 # Make me true!
-bool_three = 
+bool_three = 3<4
 
 # Make me false!
-bool_four = None
+bool_four = 
 
 # Make me true!
 bool_five = None
