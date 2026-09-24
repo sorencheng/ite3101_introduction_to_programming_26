a32bbcd925ce103efc0e51229b25aa20cd0ec149
sorencheng <sorencheng2002@gmail.com>
2026-09-24 08:11:50 +0000

    [skip actions] lab/lab03/ch03_t04_hot_date.py

--- lab/lab03/ch03_t04_hot_date.py
+++ lab/lab03/ch03_t04_hot_date.py
@@ -4,4 +4,4 @@ now = datetime.now()
 current_month = now.month
 current_year = now.year
 current_day = now.day
-print('%02d-%/02d-%04d' % (now.month, now.day, now.year))
+print('%02d-%02d-%04d' % (now.month, now.day, now.year))
