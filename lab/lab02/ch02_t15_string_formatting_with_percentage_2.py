cb6c877cf1236832d76f72fc4c5bfdfdca5f82d0
sorencheng <sorencheng2002@gmail.com>
2026-09-24 07:19:27 +0000

    [skip actions] lab/lab02/ch02_t15_string_formatting_with_percentage_2.py

--- lab/lab02/ch02_t15_string_formatting_with_percentage_2.py
+++ lab/lab02/ch02_t15_string_formatting_with_percentage_2.py
@@ -3,5 +3,5 @@ quest = " Happy "
 color =  38 
 
 # Uncomment the below 2 line of code!
- print("Ah, so your name is %s, your quest is %s, "
- "and your favorite color is %s." ___ (name, quest, color))
+ print("Ah, so your name is %s , your quest is %s , "
+ "and your favorite color is %s ." ___ (name, quest, color))
