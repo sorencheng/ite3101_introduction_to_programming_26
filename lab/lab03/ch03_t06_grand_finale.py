e1f2eb9b5e88fcfaf2b86f418608b855711f996c
sorencheng <sorencheng2002@gmail.com>
2026-09-24 08:21:06 +0000

    [skip actions] lab/lab03/ch03_t06_grand_finale.py

--- lab/lab03/ch03_t06_grand_finale.py
+++ lab/lab03/ch03_t06_grand_finale.py
@@ -5,6 +5,6 @@ current_month = now.month
 current_day = now.day
 current_year = now.year
 current_hour = now.hour
-current_
+current_minute 
 
 print('%02d:%02d:%04d' % (now.hour, now.minute, now.second))
