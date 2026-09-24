24269da7379d27891b4fab6762f14f4cf1590e38
sorencheng <sorencheng2002@gmail.com>
2026-09-24 07:20:13 +0000

    [skip actions] lab/lab02/ch02_t15_string_formatting_with_percentage_2.py

--- lab/lab02/ch02_t15_string_formatting_with_percentage_2.py
+++ lab/lab02/ch02_t15_string_formatting_with_percentage_2.py
@@ -4,4 +4,4 @@ color = 38
 
 # Uncomment the below 2 line of code!
  print("Ah, so your name is %s , your quest is %s , "
- "and your favorite color is %s ." ___(Cyrus, Happy, 38)
+ "and your favorite color is %s ."    (Cyrus, Happy, 38)
